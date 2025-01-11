26187d679e255815af01ffe3a521b17d90457cf6
mizterdy07 <mizterdy07@gmail.com>
2025-01-11 11:34:23 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -7,26 +7,50 @@ DataTable.use(DataTablesCore);
 
 const datatable = ref(null); // Table reference
 
+const tableData = ref([]);
+
 const columns = reactive([
   { data: 'userId', title: 'UserId', },
   { data: 'id', title: 'id', },
-  { data: 'body', title: 'Body' },
   { data: null, title: 'Title',render:'#title' },
 
 ]);
 
-onMounted(() => {
+const options = reactive({
+  info: true,
+  searching: true,
+  paging: true,
+  reload: true,
+})
 
+onMounted(async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts?_start=0&_limit=3');
+  const data = await response.json();
+  tableData.value = data; // Assign the fetched data to tableData
 });
+
+function sortData(direct){
+  if(direct === "DESC"){
+    tableData.value = tableData.value.sort((a,b)=>b.id-a.id)
+  }else if(direct ==="ASC"){
+    tableData.value = tableData.value.sort((a,b)=>a.id-b.id)
+
+  }
+}
 </script>
 
 <template>
   <div class="p-6">
-    <div class="rounded-md bg-white p-4 shadow-sm">
+    <div class="group-btn">
+      <button @click="sortData('DESC')">Sort DESC</button>
+      <button @click="sortData('ASC')">Sort ASC</button>
+    </div>
+    <div class="container-table">
       <DataTable
         ref="datatable"
         :columns="columns"
-        ajax="https://jsonplaceholder.typicode.com/posts?_start=0&_limit=3"
+        :options="options"
+        :data="tableData"
         class="display nowrap" width="100%"
       >
      <template #title="{rowData}">
@@ -38,3 +62,12 @@ onMounted(() => {
 
   </div>
 </template>
+
+<style>
+.group-btn{
+  display: flex;
+  align-items: center;
+  gap: 6px;
+}
+
+</style>
