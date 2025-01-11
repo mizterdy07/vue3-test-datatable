bb5442186d3d73172a4294bbf05ab0a77a424247
mizterdy07 <mizterdy07@gmail.com>
2025-01-11 18:12:34 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,34 @@ DataTable.use(DataTablesCore);
 
 const datatable = ref(null); // Table reference
 
-const tableData = ref([]);
+const ajax = reactive({
+  url:'https://jsonplaceholder.typicode.com/comments?postId=1',
+  data:(data)=>{
+    console.log('---->>>',data)
+    const order = data.order;
+    if (order.length) {
+    const columns = data.columns[order[0].column];
+
+    if (columns.orderable && columns.data) {
+      data.OrderBy = columns.data;
+      data.OrderDir = order[0].dir.toUpperCase();
+      return {
+        OrderBy:columns.data,
+        OrderDir:order[0].dir
+      }
+    }
+  }
+  },
+  dataSrc:(dataSrc)=>{
+    return dataSrc
+  }
+})
 
 const columns = reactive([
-  { data: 'userId', title: 'UserId', },
-  { data: 'id', title: 'id', },
-  { data: null, title: 'Title',render:'#title' },
+  { data: 'id', title: 'Id', },
+  { data: 'name', title: 'Name',render:'#name' },
+  { data: 'email', title: 'Email', },
+  { data: 'phone', title: 'Phone', },
 
 ]);
 
@@ -21,41 +43,30 @@ const options = reactive({
   searching: true,
   paging: true,
   reload: true,
+  processing : true,
+  serverSide : true,
+  debug : true,
 })
 
 onMounted(async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts?_start=0&_limit=3');
-  const data = await response.json();
-  tableData.value = data; // Assign the fetched data to tableData
+
 });
 
-function sortData(direct){
-  if(direct === "DESC"){
-    tableData.value = tableData.value.sort((a,b)=>b.id-a.id)
-  }else if(direct ==="ASC"){
-    tableData.value = tableData.value.sort((a,b)=>a.id-b.id)
-
-  }
-}
 </script>
 
 <template>
   <div class="p-6">
-    <div class="group-btn">
-      <button @click="sortData('DESC')">Sort DESC</button>
-      <button @click="sortData('ASC')">Sort ASC</button>
-    </div>
     <div class="container-table">
       <DataTable
         ref="datatable"
         :columns="columns"
         :options="options"
-        :data="tableData"
+        :ajax="ajax"
         class="display nowrap" width="100%"
       >
-     <template #title="{rowData}">
-      <span>{{ rowData.id }}</span>
-      <span>{{ rowData.title }}</span>
+     <template #name="{rowData}">
+      <span>{{ rowData.name }}</span>
+      <span>{{ rowData.username }}</span>
     </template>
       </DataTable>
     </div>
